9ec71cf2ee1dd8079f7e9841be305a5366974737
sdx <softdirex@gmail.com>
2019-01-20 22:52:46 -0300

    [VDEV]menu de inventario

--- src/main/java/cl/softdirex/enubase/view/notifications/panels/menu/OpanelInventario.java
+++ src/main/java/cl/softdirex/enubase/view/notifications/panels/menu/OpanelInventario.java
@@ -80,24 +80,25 @@ public class OpanelInventario extends javax.swing.JPanel {
         cboOption.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI Light", 1, 14)); // NOI18N
-        jLabel3.setText("Configurar");
+        jLabel3.setText("Opciones");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(38, Short.MAX_VALUE)
+                .addContainerGap(49, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnCancel)
                         .addGap(18, 18, 18)
-                        .addComponent(btnLoad))
+                        .addComponent(btnLoad)
+                        .addGap(87, 87, 87))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel3)
-                        .addGap(67, 67, 67)
-                        .addComponent(cboOption, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(87, 87, 87))
+                        .addGap(18, 18, 18)
+                        .addComponent(cboOption, javax.swing.GroupLayout.PREFERRED_SIZE, 398, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
